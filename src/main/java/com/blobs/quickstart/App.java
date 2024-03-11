9b06d18fb86efeb954bb7503071bfb93892f94b2
Sankara Narayanan <sanaraya@microsoft.com>
2024-03-11 12:53:40 +0530

    Updated for Keyvault access

--- src/main/java/com/blobs/quickstart/App.java
+++ src/main/java/com/blobs/quickstart/App.java
@@ -1,82 +1,79 @@
 package com.blobs.quickstart;
 
-/**
- * Hello world!
- *
- */
 import com.azure.identity.*;
-import com.azure.storage.*;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
-import com.azure.storage.blob.*;
 import com.azure.storage.blob.specialized.*;
-import com.azure.storage.blob.models.*;
 import java.io.*;
-import java.nio.*;
-import java.nio.file.*;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
+
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
 
 public class App {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello World!");
+        System.out.println("App started!");
 
-        /*
-         * The default credential first checks environment variables for configuration
-         * If environment configuration is incomplete, it will try managed identity
+        /**
+         * Read Config values
          */
-        DefaultAzureCredential defaultCredential = new DefaultAzureCredentialBuilder().build();
+        String TENANT_ID = System.getenv("TENANT_ID");
+        String CLIENT_ID = System.getenv("CLIENT_ID");
+        String CLIENT_SECRET = System.getenv("CLIENT_SECRET");
+        String KEY_VAULT = System.getenv("KEY_VAULT");
+        String KEY_VAULT_SECRET_NAME = System.getenv("KEY_VAULT_SECRET_NAME");
+
+        System.err.println("---------- Config Values ----------------");
+        System.err.println("TENANT_ID :: " + TENANT_ID);
+        System.err.println("CLIENT_ID :: " + CLIENT_ID);
+        System.err.println("CLIENT_SECRET :: " + CLIENT_SECRET);
+        System.err.println("KEY_VAULT :: " + KEY_VAULT);
+        System.err.println("KEY_VAULT_SECRET_NAME :: " + KEY_VAULT_SECRET_NAME);
+
+        /**
+         * Get secret from KeyVault to access Storage account
+         */
+        ClientSecretCredential clientSecretCredential = new ClientSecretCredentialBuilder()
+                .clientId(CLIENT_ID)
+                .clientSecret(CLIENT_SECRET)
+                .tenantId(TENANT_ID)
+                .build();
+
+        SecretClient secretClient = new SecretClientBuilder()
+                .vaultUrl(KEY_VAULT)
+                .credential(clientSecretCredential)
+                .buildClient();
+
+        KeyVaultSecret retrievedSecret = secretClient.getSecret(KEY_VAULT_SECRET_NAME);
+        System.out.println("Your secret's value is '" + retrievedSecret.getValue() + "'.");
+
+        /**
+         * Get a file in Blob storage and update some data using the SAS Key obtained
+         * above
+         */
 
         // Azure SDK client builders accept the credential as a parameter
         BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
                 .endpoint("https://sanarayashellstorageacc.blob.core.windows.net/")
-                .credential(defaultCredential)
+                .sasToken(retrievedSecret.getValue())
                 .buildClient();
 
-        // Create a unique name for the container
-        String containerName = "articles";// + java.util.UUID.randomUUID();
-
-        // Create the container and return a container client object
-        BlobContainerClient blobContainerClient = blobServiceClient.getBlobContainerClient(containerName);
-
-        System.out.println("\nListing blobs...");
-
-        // List the blob(s) in the container.
-        for (BlobItem blobItem : blobContainerClient.listBlobs()) {
-            System.out.println("\t" + blobItem.getName());
-        }
-
+        // Get the container and append to an existing blob
+        String containerName = "articles";
         BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
         AppendBlobClient appendBlob = containerClient.getBlobClient("abcd1.txt").getAppendBlobClient();
 
-        // appendBlob.create(true);
-        // java.util.Random random = new java.util.Random();
-        // File tempFile1 = DataGenerator.createTempLocalFile("appendblob-", ".tmp",
-        // (128 * 1024) + random.nextInt(128 * 1024));
-
-        // appendBlob.getBlobOutputStream().write("asdfdsaf".getBytes());
-
-        String wd = "\n" + "test" + Math.random();
-        //
-        // AppendBlobClient appendBlobOrig = containerClient.getBlobClient("Article
-        // 1.txt").getAppendBlobClient();
-        try (InputStream inputStream = new ByteArrayInputStream(wd.getBytes(StandardCharsets.UTF_8))) {
+        // Append some dummy data
+        String wd = "\n" + "Entry" + System.currentTimeMillis();
+        try (InputStream inputStream = new ByteArrayInputStream(
+                wd.getBytes(StandardCharsets.UTF_8))) {
             // Assuming blob is some object that has the appendFromStream method
             appendBlob.appendBlock(inputStream, wd.length());
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        // FileChannel fileChannel = FileChannel.open(tempFile1.toPath());
-        // ByteBuffer fileByteBuffer =
-        // ByteBuffer.allocate(Long.valueOf(fileChannel.size()).intValue());
-        // fileChannel.read(fileByteBuffer);
-        // fileChannel.close();
-        // appendBlob.getBlobOutputStream().write(fileByteBuffer.array());
-        // fileByteBuffer.clear();
-
     }
 }
